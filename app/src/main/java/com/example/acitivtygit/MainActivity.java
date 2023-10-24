832960fed67eb81fbcd9e601715dc17f511f8f8d
Jotlann <jolopezaraya05@gmail.com>
2023-10-24 16:21:22 -0300

    Initial Commit

---
+++ app/src/main/java/com/example/acitivtygit/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.acitivtygit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("El pozole");
+
+        System.out.println("El pozole");
+
+        System.out.println("El pozole");
+
+        System.out.println("El pozole");
+
+    }
+}
